3e5bc968c1cc0edd08dc7500b972f680c26b14ee
blue cat <01claudia.parravano@gmail.com>
2023-06-07 17:58:27 +0200

    first commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useEffect, useState } from "react";
+
+interface Task {
+  id: string;
+  title: string;
+  desc: string;
+  due_date: number;
+  priority: string;
+  tags: string[];
+}
+
+async function fetchData(url: string): Promise<Task[]> {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default function TasksPage(): JSX.Element {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const fetchedTasks = await fetchData("http://localhost:3005/tasks");
+        setTasks(fetchedTasks);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+    fetchTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ server/index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import { connectToDb, getDatabase } from "./mongodb/database.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let database;
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3005, () => {
+      console.log("app listening on port 3005");
+    });
+    database = getDatabase();
+  }
+});
+
+app.get("/users", (req, res) => {
+  let users = [];
+  database
+    .collection("users")
+    .find()
+    .sort({ title: 1 })
+    .forEach((user) => users.push(user))
+    .then(() => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "could not fetch users" });
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  let tasks = [];
+  database
+    .collection("tasks")
+    .find()
+    .sort({ title: 1 })
+    .forEach((task) => tasks.push(task))
+    .then(() => {
+      res.status(200).json(tasks);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "could not fetch tasks" });
+    });
+});
